dd7068f88245065f634ed9f4972cad9a3ee838cd
Ashwani Mishra <220293739+ashwyni-mishra@users.noreply.github.com>
2026-05-17 01:08:32 +0530

    chore: clear all files for new repository structure

--- clean_install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Cleaning corrupted node_modules and lockfiles...
-rmdir /s /q node_modules
-del /q package-lock.json
-echo Reinstalling dependencies cleanly...
-npm install
-echo Finished! You can now run "npm run dev".
-pause
